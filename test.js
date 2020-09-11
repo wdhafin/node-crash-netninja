75b02e0a9054f224082163fb75bb5659f7e7f597
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-11 21:06:41 +0700

    add sample function

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-const name = 'Dhafin';
+// const name = 'Dhafin';
 
-console.log(name);
+// console.log(name);
+
+const greet = (name) => {
+  console.log(`Hello, ${name}`);
+};
+
+greet('Mario');
+greet('Dhafin');

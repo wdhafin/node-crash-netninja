34d2033b91c6e2e627ca6363fa76e82c0130deaa
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-12 00:47:55 +0700

    Add redirect and 404 using Express

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ app.get('/about', (req, res) => {
   //res.send('<p>about page</p>');
   res.sendFile('./views/about.html', { root: __dirname });
 });
+
+// redirect
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', { root: __dirname });
+});

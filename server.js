f6079631420d35db9f275ce0f5d32176f4810447
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-11 22:52:33 +0700

    Add simple response example

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  //set header content type
+  res.setHeader('Contect-Type', 'text/html');
+
+  res.write('<head><link rel="stylesheet" href="#"></head>');
+  res.write('<p>Hello, dhafin</p>');
+  res.write('<p>Hello Again, dhafin</p>');
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {

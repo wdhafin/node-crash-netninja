0c2971a5db5331fb5d305e6ef88190d15ff5868d
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-12 11:55:28 +0700

    EJS View Engine

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ const express = require('express');
 //express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for request
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  //res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
   //res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about');
 });
 
-// redirect
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create');
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404');
 });

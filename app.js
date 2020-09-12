30005e5a2e61989ecd7566df953b5e96f1751bb2
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-12 23:58:17 +0700

    Add Mongoose and Add New Model

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 //express app
 const app = express();
 
 // connect to mongoDB
 const dbURI =
-  'mongodb+srv://dhafin:Goliathus123-@dhafin-node.eu5nt.mongodb.net/<dbname>?retryWrites=true&w=majority';
+  'mongodb+srv://dhafin:Goliathus123-@dhafin-node.eu5nt.mongodb.net/node?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // listen for request
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));

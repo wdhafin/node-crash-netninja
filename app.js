f296b8efff7e1123cba50f2bd8eb22f6036f102c
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-12 23:10:45 +0700

    Add static file example

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.set('view engine', 'ejs');
 // listen for request
 app.listen(3000);
 
+// middleware & static files
+app.use(express.static('public'));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {

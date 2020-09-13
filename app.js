843408e66b1f9645f9132380c67ef07953574fa8
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-13 22:16:59 +0700

    Add Route Parameters
    - add route params in node
    - add detail page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
 const { result } = require('lodash');
+const { render } = require('ejs');
 
 //express app
 const app = express();
@@ -90,6 +91,17 @@ app.post('/blogs', (req, res) => {
     .save()
     .then((result) => {
       res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render('details', { blog: result, title: 'Blog Details' });
     })
     .catch((err) => {
       console.log(err);
